fad7573ab54bc3c83abb04af900833ca88ccc603
LivieGit <livinjoroge@gmail.com>
2025-01-01 01:05:16 +0300

    future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,14 @@
+# Future age calculator
+
+# Get user's current age
+current_age = int(input("How old are you? "))
+
+# Target year
+target_year = 2050
+# Assuming current year is 2023
+
+# Calculate age in target year
+age_in_target_year = current_age + (target_year - 2023)
+
+# Print the result
+print(f"In {target_year}, you will be {age_in_target_year} years old.")
